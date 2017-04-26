09a9082ca17df8f527327efdcce11961ae675aa9
danielliauw <danielliauw@hotmail.co.id>
2017-04-26 15:28:25 +0800

    add downvote and upvote

--- Reddit.py
+++ Reddit.py
@@ -3,9 +3,17 @@ app = Flask(__name__)
 
 postList = {'test': 1}
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def home():
-    return render_template('home.html',posts = postList)
+	if request.method == 'POST':
+		if "upvote" in request.form:
+			postList[request.form['upvote']] +=1
+			return redirect(url_for('home'))
+		elif "downvote" in request.form:
+			postList[request.form['downvote']] -=1
+			return redirect(url_for('home'))
+	sorted_posts = sorted(postList.items(), key = lambda x:x[1], reverse = True)[:20]
+	return render_template('home.html',posts = sorted_posts)
 
 @app.route('/submit_post',methods = ['GET','POST'])
 def submit_post():
